4be8c6796bd53b84c81c2b3362249a523fde1ed9
zxr <1523871672@qq.com>
2026-02-25 22:51:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# R-bot
+
+- 基于Nonebot2框架的官方QQ机器人
+- 目标
+  - [ ] maya api的查询
+
+---
+
+## 环境部署
+
+- `uv sync`拉取依赖
+- 根据`.env.example` 建立 `.env.prod` 文件并在其中填写**appid**、**secret**等机器人信息
+- `uv run nb run`或`uv run nb run --reload`启动机器人
+
+---
+
+## 命令说明
+
+- `/echo`
+  - 内置插件，回显消息
+- `/random [max-number]`
+  - 生成以max-number为上限的随机正数，如`/random 100`
+
+---
+
+## 开发说明
+
+- 提交代码前使用`pyright`/`ruff`进行代码检查与格式化
+  ```shell
+  uv run ruff check .
+  uv run ruff format .
+  uv run pyright
+  # 或 uv run python -m pyright
+  ```
---
+++ src/plugins/myplugin/qq_authorization.py
@@ -0,0 +1,31 @@
+import nonebot
+import requests
+
+config = nonebot.get_driver().config
+
+secret = config.qq_bots[0]["secret"]
+appId = config.qq_bots[0]["id"]
+token_get_url = config.qq_bots[0]["token_get_url"]
+
+
+def get_access_token():
+    """
+    使用appid和secret获取qq开放平台的access_token
+    """
+    headers = {"Content-Type": "application/json"}
+    data = {"appId": appId, "clientSecret": secret}
+
+    # TODO:容错机制
+    response = requests.post(token_get_url, headers=headers, json=data)
+    token = response.json()["access_token"]
+    return token
+
+
+def get_authorization_headers():
+    token = get_access_token()
+    authorization_headers = {
+        "Authorization": f"QQBot {token}",
+        "X-Union-Appid": appId,
+    }
+
+    return authorization_headers
